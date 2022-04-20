9558cb73f378fe00ea7bf294649a5a012b7cab4d
SerKo <serko.dev@gmail.com>
2022-04-21 03:45:28 +0800

    init

---
+++ src/components/Monaco.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import * as monaco from "monaco-editor";
+import { onMounted, ref } from "vue";
+
+const root = ref<HTMLElement>();
+
+onMounted(() => {
+  root.value && monaco.editor.create(root.value);
+});
+</script>
+
+<template>
+  <div ref="root"></div>
+</template>
