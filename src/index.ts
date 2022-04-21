f50af7be65c8922e3cb89db9d39df5f9ac327397
SerKo <serko.dev@gmail.com>
2022-04-22 01:23:56 +0800

    feat: add css parser dependency

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-globalThis.console.log('hi')
+import css = require('css')
+
+const obj = css.parse(`body {
+    font-size: 12px !important;
+    color: red;
+}`)
+
+obj.stylesheet.rules.forEach((rule: css.Rule) => {
+  console.log(rule.declarations)
+})
