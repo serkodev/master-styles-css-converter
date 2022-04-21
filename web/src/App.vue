36b5a9f1adcc00fe281f414a59b064932c70199c
SerKo <serko.dev@gmail.com>
2022-04-22 00:29:47 +0800

    refactor: remove unused code

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,19 +2,18 @@
 import '@master/normal.css'
 import '@master/styles'
 import { ref } from 'vue'
-import { foo } from 'master-styles-css-converter'
 import Manaco from './components/Monaco/index.vue'
 
 const css = ref(`body {
     background: red;
 }`)
-
-const t = ref(foo)
 </script>
 
 <template>
   <div class="d:grid grid-cols:2 h:100vh">
-    <Manaco :value="css" @update:value="css = $event" />
-    <div>{{ css }} {{ t }}</div>
+    <Manaco :value="css" @update:value="css = $event">
+      <div>{{ css }}</div>
+    </Manaco>
+    <div>{{ css }}</div>
   </div>
 </template>
